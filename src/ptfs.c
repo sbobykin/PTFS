cf0ffebf9c5fcfc5aac0c30e76b9c01f12a171de
Stanislav Bobykin <sbobykin@gmail.com>
2009-06-16 15:01:36 +0600

    Function 'get_node' was moved from 'src/ptfs.c' to 'src/parse.c'. The target 'ptfs: *.o' for linking was changed to the analogous target that is generated by automake.

--- src/ptfs.c
+++ src/ptfs.c
@@ -46,6 +46,7 @@ static struct options {
 	int version;
 } options;
 
+
 #define PTFS_OPT_KEY(t, p, v) { t, offsetof(struct options, p), v }
 
 static struct fuse_opt ptfs_opts[] = {
@@ -61,9 +62,6 @@ static struct fuse_opt ptfs_opts[] = {
 };
 
 
-#define isDir 1
-#define isText 2
-#define unknown 0
 
 void print_usage(FILE *F, int exit_code)
 {
@@ -82,78 +80,6 @@ void print_version()
 {
 	printf("ptfs version 0.0.2.1\n");
 	exit(0);
-}
-
-
-int get_node(const char* path, tree* ret_tr, struct pars_obj** ret_pars_obj)
-{
-	int i;
-	int status;
-	int nd_choosen = 1;
-	char symb_pos[4];
-	char* tok;
-	tree cur_tr = tr;
-	node cur_nd;
-	struct pars_obj* pars_obj;
-	
-	*ret_tr = tr;
-	tok = strtok(path, "/");
-	if(tok!=NULL) {
-		pars_obj = g_hash_table_lookup(files, tok);
-		*ret_pars_obj = pars_obj;
-		if(pars_obj) {
-			cur_tr = pars_obj->tr;
-			*ret_tr = cur_tr;
-		}
-		else {
-			status = unknown;
-			goto out;
-		}
-	}
-	tok = strtok(NULL, "/");
-	//printf("tok is %s\n", tok);
-	while(tok) {
-
-		nd_choosen = 0;
-
-		if(strcmp(tok, "text") == 0) {
-			status = isText;
-			goto out;
-		}
-
-		snprintf(symb_pos, 4, "%s", tok);
-		
-		cur_tr = cur_tr->t_element.t_node.n_children;
-		if(!cur_tr) {
-			status = isDir;
-			goto out;
-		}
-
-		for(i = 0;
-		    cur_tr; 
-		    cur_tr = cur_tr->t_sibling, i++) {
-			cur_nd = &(cur_tr->t_element.t_node);
-			if(strcmp(tok+4, cur_nd->n_name) == 0 &&
-			    i == atoi(symb_pos) ) {
-				*ret_tr = cur_tr;
-				nd_choosen = 1;
-				break;
-			}
-		}
-
-
-		if(!nd_choosen) {
-			status = unknown;
-			goto out;
-		}
-
-		
-		tok = strtok(NULL, "/");
-	}
-	status = isDir;
-
-out:
-	return status;
 }
 
 static int ptfs_getattr(const char *path, struct stat *stbuf)
--- src/ptfs.h
+++ src/ptfs.h
@@ -1,6 +1,10 @@
 #include <glib.h>
 #include <parse_tree.h>
 
+#define isDir 1
+#define isText 2
+#define unknown 0
+
 GHashTable* files;
 tree tr;
 
@@ -13,3 +17,4 @@ struct pars_obj {
 char* grammar;
 char* map_file_to_str (char* pathname, int* size);
 int parse_file(char* file_name);
+int get_node(const char* path, tree* ret_tr, struct pars_obj** ret_pars_obj);
