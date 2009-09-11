2d0de2d41460b4d35d49269113123687cd539cf2
Stanislav Bobykin <sbobykin@gmail.com>
2009-09-11 16:54:58 +0600

    Removing of the 'aurochs/' directory.

--- aurochs/aurochs/compiler.mli
+++
@@ -1,3 +0,0 @@
-(* Compiler *)
-
-val compile : ?start:string -> ?base:string -> ?root:string -> ?check:bool -> string -> Aurochs.binary
--- aurochs/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-ocamlbuild -cflags -w,P aurochs_tool.native $@
